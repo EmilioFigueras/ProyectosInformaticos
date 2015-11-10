66cd48a7830df6bb769c8d2a4ce6a93b25bde7b1
Emilio Figueras <emilio@emilio.debian>
2015-11-10 21:06:53 +0100

    Mod 10/11 (1)

--- Asignatura.java
+++ Asignatura.java
@@ -53,28 +53,28 @@ public class Asignatura{
 	//Double?? O tipo de dato que sea hora_comienzo en la clase Teoria
 	//Indica la hora de comienzo del grupo "gr" (empieza en 0)
 	public double hora_inicio_t(int gr){
-		return teoria_.get(gr).hora_comienzo;
+		return teoria_.get(gr).hora_inicio();
 	}
 
 	public double hora_inicio_p(int gr){
-		return practica_.get(gr).hora_comienzo;
+		return practica_.get(gr).hora_inicio();
 	}
 
 	public double hora_inicio_s(int gr){
-		return seminario_.get(gr).hora_comienzo;
+		return seminario_.get(gr).hora_inicio();
 	}
 
 	//Double? o tipo de dato que sea hora_fin en la clase Toeria/Pr/Sem
 	public double hora_fin_t(int gr){
-		return teoria_.get(gr).hora_fin;
+		return teoria_.get(gr).hora_fin();
 	}
 
 	public double hora_fin_p(int gr){
-		return practica_.get(gr).hora_fin;
+		return practica_.get(gr).hora_fin();
 	}
 
 	public double hora_fin_s(int gr){
-		return seminario_.get(gr).fin;
+		return seminario_.get(gr).fin();
 	}
 
 
---
+++ Teoria.java
@@ -0,0 +1,49 @@
+ public class Teoria{
+ 	String codigoAsig_;
+ 	String grupo_;
+ 	double hora_inicio_;
+ 	double hora_fin_;
+
+ 	public Teoria(String codigoAsig, String grupo, double hora_inicio, double hora_fin){
+ 		codigoAsig_ = codigoAsig_;
+ 		grupo_ = grupo;
+ 		hora_inicio_ = hora_inicio;
+ 		hora_fin_ = hora_fin;
+ 	}	
+
+ 	//Observadores
+ 	public double hora_inicio(){
+ 		return hora_inicio_;
+ 	}
+
+ 	public double hora_fin(){
+ 		return hora_fin_;
+ 	}
+
+ 	public String codigoAsig(){
+ 		return codigoAsig_;
+ 	}
+
+ 	public String grupo(){
+ 		return grupo_;
+ 	}
+
+ 	//Modificadores
+ 	public void hora_inicio(double hora_inicio){
+ 		hora_inicio_ = hora_inicio;
+ 	}
+
+ 	public void hora_fin(double hora_fin){
+ 		hora_fin_ = hora_fin;
+ 	}
+
+ 	public void codigoAsig(String codigoAsig){
+ 		codigoAsig_ = codigoAsig;
+ 	}
+
+ 	public void grupo(String grupo){
+ 		grupo_ = grupo;
+ 	}
+
+
+ }
